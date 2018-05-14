ad62d4074a70ed482dc2a3055d9417febd2dd286
Kaz Okuda <kaokuda@microsoft.com>
2018-05-14 13:43:33 -0700

    MacOS and iOS - Added build scripts to generate NuGet packages
    * Generate and build MacOS, iOS, and iOS simulator builds
    * Generate MacOS and iOS NuGet packages

---
+++ cmake/GenerateMacOSiOS.ps1
@@ -0,0 +1,100 @@
+<#
+.SYNOPSIS
+Generate the MacOS, iOS, and iOS Simulator projects from the cmake scripts
+
+.DESCRIPTION
+Generate the MacOS, iOS, and iOS Simulator projects from the cmake scripts.
+Use BuildMacOSiOSNuGet.ps1 to build and generate the nuget packages.
+
+
+.EXAMPLE
+GenerateMacOSiOS.ps1
+#>
+
+[CmdletBinding()]
+param(
+    [switch]$Clean,
+    [switch]$NoMacOS,
+    [switch]$NoIOS,
+    [switch]$NoSimulator
+)
+
+$MACOS_OUTPUT_FOLDER = "macos"
+$IOS_OUTPUT_FOLDER = "ios"
+$IOS_SIMULATOR_OUTPUT_FOLDER = "ios_simulator"
+
+function CleanFiles()
+{
+    Remove-Item "$PSScriptRoot/../build" -Recurse -Force | Write-Host
+}
+
+function GenerateProjectMacOS()
+{
+    Write-Host "Generate XCode Project"
+    New-Item -Path "$PSScriptRoot/../build" -Name $MACOS_OUTPUT_FOLDER  -ItemType Directory -Force | Out-Null
+    Push-Location "$PSScriptRoot/../build/$MACOS_OUTPUT_FOLDER"
+    try
+    {
+      cmake -G Xcode ../..
+    }
+    finally
+    {
+      Pop-Location
+    }
+}
+
+function GenerateProjectIOS()
+{
+    Write-Host "Generate XCode Project"
+    New-Item -Path "$PSScriptRoot/../build" -Name $IOS_OUTPUT_FOLDER -ItemType Directory -Force | Out-Null
+    Push-Location "$PSScriptRoot/../build/$IOS_OUTPUT_FOLDER" | Out-Null
+    try
+    {
+      cmake -G Xcode ../.. -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/ios.toolchain.cmake" -DIOS_PLATFORM=OS -DIOS_DEPLOYMENT_TARGET="9.0" | Write-Host
+    }
+    finally
+    {
+      Pop-Location | Out-Null
+    }
+}
+
+function GenerateProjectIOSSimulator()
+{
+    Write-Host "Generate XCode Project"
+    New-Item -Path "$PSScriptRoot/../build" -Name $IOS_SIMULATOR_OUTPUT_FOLDER -ItemType Directory -Force | Out-Null
+    Push-Location "$PSScriptRoot/../build/$IOS_SIMULATOR_OUTPUT_FOLDER" | Out-Null
+    try
+    {
+      cmake -G Xcode ../.. -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/ios.toolchain.cmake" -DIOS_PLATFORM=SIMULATOR64 -DIOS_DEPLOYMENT_TARGET="9.0" | Write-Host
+    }
+    finally
+    {
+      Pop-Location | Out-Null
+    }
+}
+
+function Main()
+{
+    if ($Clean)
+    {
+        CleanFiles                
+    }
+
+    if (!$NoMacOS)
+    {
+        GenerateProjectMacOS
+    }
+
+    if (!$NoIOS)
+    {
+        GenerateProjectIOS
+    }
+
+    if (!$NoSimulator)
+    {
+        GenerateProjectIOSSimulator
+    }
+    
+}
+
+Main
---
+++ nuget/ios/FindLib3MF.iOSSimulator64.cmake
@@ -0,0 +1,11 @@
+cmake_minimum_required(VERSION 3.9)
+
+if (NOT Lib3MF_IOSSIMULATOR64_FOUND)
+    set(Lib3MF_IOSSIMULATOR64_FOUND TRUE)
+    add_library(Lib3MF STATIC IMPORTED GLOBAL)
+    set_target_properties(Lib3MF PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/build/native/include")
+    set_target_properties(Lib3MF PROPERTIES IMPORTED_LOCATION_DEBUG "${CMAKE_CURRENT_LIST_DIR}/build/native/lib/iOSSimulator64/Debug/static/lib3MF_s.a")
+    set_target_properties(Lib3MF PROPERTIES IMPORTED_LOCATION_RELEASE "${CMAKE_CURRENT_LIST_DIR}/build/native/lib/iOSSimulator64/Release/static/lib3MF_s.a")
+    set_target_properties(Lib3MF PROPERTIES IMPORTED_LOCATION_RELWITHDEBINFO "${CMAKE_CURRENT_LIST_DIR}/build/native/lib/iOSSimulator64/Release/static/lib3MF_s.a")
+    set_target_properties(Lib3MF PROPERTIES IMPORTED_LOCATION_MINSIZEREL "${CMAKE_CURRENT_LIST_DIR}/build/native/lib/iOSSimulator64/Release/static/lib3MF_s.a")
+endif()
