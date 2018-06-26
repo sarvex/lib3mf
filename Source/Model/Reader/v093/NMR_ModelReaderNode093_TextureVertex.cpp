57d5fd4d5fb8063c8e46ddf9f05222b8f290f2f8
Kaz Okuda <kaokuda@microsoft.com>
2018-06-26 11:24:41 -0700

    Removed error checking when reading vertext attributes
    * The error checking was preventing older files with bad vertices from being loaded
    * It is up the application to sanitize the data before rendering it.

--- Source/Model/Reader/v093/NMR_ModelReaderNode093_TextureVertex.cpp
+++ Source/Model/Reader/v093/NMR_ModelReaderNode093_TextureVertex.cpp
@@ -86,19 +86,11 @@ namespace NMR {
 
 		if (wcscmp(pAttributeName, XML_3MF_ATTRIBUTE_TEXTURE_U) == 0) {
 			m_fU = wcstof(pAttributeValue, nullptr);
-			if (std::isnan (m_fU))
-				throw CNMRException(NMR_ERROR_INVALIDMODELTEXTURECOORDINATES);
-			if (fabs (m_fU) > XML_3MF_MAXIMUMCOORDINATEVALUE)
-				throw CNMRException(NMR_ERROR_INVALIDMODELTEXTURECOORDINATES);
 			m_bHasU = true;
 		}
 
 		if (wcscmp(pAttributeName, XML_3MF_ATTRIBUTE_TEXTURE_V) == 0) {
 			m_fV = wcstof(pAttributeValue, nullptr);
-			if (std::isnan (m_fV))
-				throw CNMRException(NMR_ERROR_INVALIDMODELTEXTURECOORDINATES);
-			if (fabs(m_fV) > XML_3MF_MAXIMUMCOORDINATEVALUE)
-				throw CNMRException(NMR_ERROR_INVALIDMODELTEXTURECOORDINATES);
 			m_bHasV = true;
 		}
 
